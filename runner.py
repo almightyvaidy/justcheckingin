708dd48d2f2c0284e7122efa3fb069b9788d7faa
Vaidy Krishnamurthy <almightyvaidy@users.noreply.github.com>
2021-06-08 10:44:27 -0500

    Add files via upload

---
+++ runner.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  6 11:36:44 2021
+
+@author: vaidy
+"""
+# what the the external dat input that need to supplied to the objects instatiated using this class
+class runner:
+    def __init__(self,x):
+        return self
+    def health():
+        return
+    def sleep():
+        return
+    def strech():
+        return
+    def run():
+        # call ditance becasue the you increase distance  when you run 
+        # you get tired when you run
+        # you lose energy while running 
+        # your joints get damage when running with some probability 
+        return
+    def poop():
+        return
+    def body():
+        return 
+
+    
